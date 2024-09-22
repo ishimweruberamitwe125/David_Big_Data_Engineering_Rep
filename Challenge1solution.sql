2ec22bf479df50b34755ccc456d56eb961b68357
David Ishimwe Ruberamitwe <85899935+ishimweruberamitwe125@users.noreply.github.com>
2024-09-21 21:23:21 -0400

    Update Challenge1solution.sql

--- Challenge1solution.sql
+++ Challenge1solution.sql
@@ -1,4 +1,4 @@
-use challenge1;
+#use challenge1;
 #question1
 select s.customer_id,sum(m.price) from sales as s
 left join menu as m on s.product_id=m.product_id
@@ -15,43 +15,37 @@ from sales as s
 inner join menu as m on s.product_id=m.product_id)
 select * from first_product where row_first=1;
 #---------------------------------------------------------------
-#question4
-select m.product_name,count(s.product_id) as purchase_frequency
-from sales as s
-join menu as m on s.product_id=m.product_id
-group by m.product_name
-order by purchase_frequency desc limit 1;
+#question 4
+select m.product_name, purchases.total_purchases
+from (select product_id, COUNT(*) AS total_purchases
+from sales 
+group by product_id
+order by total_purchases desc limit 1
+    ) AS purchases
+join menu m on purchases.product_id = m.product_id;
+
+
 #------------------------------------------------------------------------------
 #question 5
-With rank1 as
-(
-Select s.customer_id ,m.product_name, count(s.product_id) as frequency,
-dense_rank()  Over (Partition by s.customer_id order by Count(S.product_id) desc ) as rank2
-From menu m
-join sales s
-On m.product_id = s.product_id
-group by s.customer_id,s.product_id,m.product_name
-)
-Select customer_id,product_name,frequency
-From rank1
-where rank2 = 1;
+select s.customer_id,m.product_name,COUNT(s.product_id) AS frequency
+from sales as s
+left join menu AS m on s.product_id = m.product_id
+group by  s.customer_id, m.product_name
+having count(s.product_id) = (#max
+select max(product_count)from(select s2.product_id,count(s2.product_id) AS product_count
+from sales AS s2 where s2.customer_id = s.customer_id group by s2.product_id) as customer_max_counts)
+order by s.customer_id;
 #-----------------------------------------------------------------
 #question 6
-With Rank1 as
-(
-Select  s.customer_id,
-        m.product_name,
-	    Dense_rank() OVER (Partition by s.customer_id Order by s.order_date) as Rank2
-From sales s
-Join menu m
-ON m.product_id = s.product_id
-JOIN Members mem
-ON mem.customer_id = s.customer_id
-Where s.order_date >= mem.join_date  
-)
-Select *
-From Rank1
-Where Rank2 = 1;
+WITH Rank1 AS (
+select s.customer_id,m.product_name,me.join_date,s.order_date
+from sales s join menu m on m.product_id = s.product_id 
+join members me on me.customer_id = s.customer_id
+where s.order_date >= me.join_date and s.order_date = (
+#subquery to find the earliest order date for each customer
+select min(s2.order_date) from sales s2 where s2.customer_id = s.customer_id
+and s2.order_date >= me.join_date))
+select * from Rank1 order by s.customer_id;
 #-------------------------------------------------------------------------
 #question 7
 With Rank1 as
@@ -67,36 +61,48 @@ Where s.order_date < mem.join_date
 Select customer_id, product_name
 From Rank1
 Where Rank2 = 1;
+#----------
+WITH Rank2 AS (
+select s.customer_id,m.product_name,me.join_date,s.order_date from sales s
+join menu m on m.product_id = s.product_id
+join members me on me.customer_id = s.customer_id where s.order_date < me.join_date
+and s.order_date =(#subquery to get the earliest order date before the join date
+select min(s2.order_date) from sales s2
+where s2.customer_id = s.customer_id and s2.order_date < me.join_date)
+)select customer_id, product_name, join_date,order_date from Rank2 order by customer_id;
 #---------------------------------------------------------------------------------
 #question 8
-select s.customer_id,count(s.product_id ) as quantity ,sum(m.price) as total_sales
-from sales s
-join menu m
-on m.product_id = s.product_id
-join members mem
-on Mem.Customer_id = S.customer_id
-where s.order_date < mem.join_date
+select s.customer_id ,count(s.product_id) as total_items_purchased,SUM(m.price) AS total_amount_spent
+from sales  s
+inner join menu  m on s.product_id = m.product_id
+inner join members  me
+on me.customer_id = s.customer_id
+where s.order_date < me.join_date
 group by s.customer_id;
+
 #-------------------------------------------------------
 #question 9
-with points_table as
-(select *, case when product_id = 1 then price*20 Else price*10 End as points
-From menu
-)
-select s.customer_id, sum(p.points) as points1
-From sales s
-Join points_table p
-On p.product_id = s.product_id
-Group by s.customer_id;
+select s.customer_id, sum(case 
+when m.product_id = 1 then m.price * 20 else m.price * 10 
+end) as points1
+from sales s
+join (select product_id, price, case 
+when product_id = 1 then price * 20 else price * 10 
+end AS points from menu) as m on s.product_id = m.product_id
+group by s.customer_id;
 #----------------------------------------------------------------------------
 #question 10
-select s.customer_id,sum(CASE when (DATEDIFF(me.join_date, s.order_date) between 0 and 7) or (m.product_ID = 1) Then m.price * 20
-else m.price * 10 end) As Points
-from members as me
-inner Join sales as s on s.customer_id = me.customer_id
-inner Join menu as m on m.product_id = s.product_id
-where s.order_date >= me.join_date and s.order_date <= CAST('2021-01-31' AS DATE)
-Group by s.customer_id;
+select s.customer_id, sum(case 
+when (datediff(me.join_date, s.order_date) between 0 AND 7) or (m.product_id = 1) 
+then m.price * 20 else m.price * 10 end) as total_points
+from members  me
+inner join sales s
+on s.customer_id = me.customer_id
+inner join menu AS m 
+on m.product_id = s.product_id
+where s.order_date >= me.join_date and s.order_date <= '2021-01-31'
+group by me.customer_id;
+
 
 
 
